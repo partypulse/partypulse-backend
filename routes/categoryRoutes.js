4a6379f341bc32d8cdb92c264ee8a6a01fc7aaff
Betttina <bettina.toth@gritacademy.se>
2024-05-04 08:14:25 +0200

    fourth commit
    put frontend and backend together in the same dir.
    frontend:
    in /src:
    - created up folders for pages
    - api folder
    - focusing on login, register and startpage.
    imported components for those pages in App.js. setView. with useState.
    - port 3000
    
    Backend:
    - port 8000
    - imported cors, morgan, express.json
    - router categories
    
    To do:
    - router user
    - token login in
    - clean up products
    - create img model

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const categoryController = require('../controllers/categoryController');
+
+const router = express.Router();
+
+// get all categories
+router.get('/categories', categoryController.getAllCategories);
+
+// create a new category
+router.post('/categories', categoryController.createCategory);
+
+// Route för att hämta en specifik kategori med dess subkategorier
+router.get('/categories/:categoryId', categoryController.getCategory);
+
+// update existing category
+router.patch('/categories/:categoryId', categoryController.updateCategory);
+
+// delete category
+router.delete('/categories/:categoryId', categoryController.deleteCategory);
+
+module.exports = router;
