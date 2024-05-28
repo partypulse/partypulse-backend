226eb792ac8c2d8a6b025aa2525697651d10df65
Betttina <bettina.toth@gritacademy.se>
2024-05-28 17:12:10 +0200

    BalloonProducts.js
    fixed endpoint to get products by category

--- router/CategoryRouter.js
+++ router/CategoryRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const categoryController = require("../controllers/categoryController");
 const { authenticate } = require("../service/AuthenticationService");
-
+const Product = require("../models/Product");
+const mongoose = require("mongoose");
 const router = express.Router();
 
 router.get(
@@ -21,5 +22,26 @@ router.delete(
   authenticate,
   categoryController.deleteCategory
 );
+
+// endpoint to get products by category
+router.get("/products/category/:categoryId", async (req, res) => {
+  try {
+    const categoryId = new mongoose.Types.ObjectId(req.params.categoryId);
+    const products = await Product.find({ mainCategory: categoryId }).populate(
+      "mainCategory subCategory"
+    );
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No products found for this category" });
+    }
+
+    res.json(products);
+  } catch (err) {
+    console.error("Error:", err.message);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
 
 module.exports = { router };
